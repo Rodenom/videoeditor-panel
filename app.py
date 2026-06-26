e1f0a03e4a613250be08f90edc0caf06cbb69dae
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2026-06-26 16:49:39 +0300

    v1.7

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 Video Editor — Нутра
 Запуск: python3 app.py
 """
-VERSION = "1.6"
+VERSION = "1.7"
 import io, hashlib
 import subprocess, sys, os, shutil, json, threading, uuid, time, webbrowser
 from http.server import HTTPServer, BaseHTTPRequestHandler
